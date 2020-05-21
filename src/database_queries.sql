6664e251de2a74057c2ada844e54d671846b9a49
csoderberg <csoderbe@gmail.com>
2020-05-21 10:24:29 -0400

    query for when pp upload stopped

--- src/database_queries.sql
+++ src/database_queries.sql
@@ -16,3 +16,14 @@ SELECT COUNT(pp_finished.id) num_pps,
 	LEFT JOIN osf_abstractprovider
 	ON pp_finished.provider_id = osf_abstractprovider.id
 	GROUP BY pp_submit_finished, osf_abstractprovider._id, date_trunc('month', pp_finished.created)
+
+/* query to get number of started, but not completed preprints, that got to each stage of the upload process */
+SELECT COUNT(osf_preprint.id) AS num_pps, action
+	FROM osf_preprint
+	LEFT JOIN (SELECT preprint_id, action, MIN(created)
+				FROM osf_preprintlog
+				WHERE created >= '2020-03-01'
+				GROUP BY preprint_id, action) AS pp_actions
+	ON osf_preprint.id = pp_actions.preprint_id
+	WHERE created >= '2020-03-01' AND machine_state = 'initial'
+	GROUP BY action
