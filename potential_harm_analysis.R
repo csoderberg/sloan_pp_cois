7f9008d27ad7063a65e2018c77ee8356f2a32506
csoderberg <csoderbe@gmail.com>
2020-05-28 12:32:52 -0400

    look at differences in perc unfinished

--- potential_harm_analysis.R
+++ potential_harm_analysis.R
@@ -11,3 +11,11 @@ osf_retrieve_node('y87ah') %>%
 complete_pps_data <- read_csv(here::here('counts_completed_pps.csv'))
 pp_action_data <- read_csv(here::here('counts_workflowsteps_unfinished_pps.csv'))
 
+# difference in percent of pps, by service, that were unfinished during - pre experiment
+complete_pps_data %>%
+  group_by(provider, timeframe) %>%
+  summarize(perc_unfinished = sum(num_pps[pp_submit_finished == 'not_finished']/sum(num_pps)), total = sum(num_pps), unfinished = sum(num_pps[pp_submit_finished == 'not_finished'])) %>%
+  group_by(provider) %>%
+  summarize(perc_diff = perc_unfinished[timeframe == 'during_exp'] - perc_unfinished[timeframe == 'pre_exp'], raw_diff = unfinished[timeframe == 'during_exp'] - unfinished[timeframe == 'pre_exp'])
+
+
