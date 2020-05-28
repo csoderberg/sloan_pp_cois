5362274b8791255de9e0d6355163294829afc8f3
csoderberg <csoderbe@gmail.com>
2020-05-28 12:06:09 -0400

    read in data

--- potential_harm_analysis.R
+++ potential_harm_analysis.R
@@ -1,4 +1,13 @@
 # loading libraries
-libary(tidyverse)
+library(tidyverse)
+library(osfr)
+library(here)
 
+# read in datafiles
+osf_retrieve_node('y87ah') %>%
+  osf_ls_files(path = 'potential_harm_assesment') %>%
+  osf_download()
+
+complete_pps_data <- read_csv(here::here('counts_completed_pps.csv'))
+pp_action_data <- read_csv(here::here('counts_workflowsteps_unfinished_pps.csv'))
 
